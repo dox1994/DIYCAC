f0927f476d46babb2c53ab00c17df43969619711
DataMonster <wangzhkevin@gmail.com>
2017-01-16 23:59:45 -0500

    remove unused files and dependencies clean up gulpfile

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/dist' ));
 
-app.get('/1', function (req, res) {
+app.get('/test', function (req, res) {
     res.send('Hello World');
 });
 
